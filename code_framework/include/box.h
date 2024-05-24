4b07a2f293571dd0defc1f105ead1ea72b0812b4
Griffith <griffithzerk@gmail.com>
2024-05-24 14:46:58 +1000

    Friction

--- code_framework/include/box.h
+++ code_framework/include/box.h
@@ -28,75 +28,97 @@ public:
     }
 
     void resolveCollision(Mass& mass) const override {
+        const float friction = 0.1; // friction : [0, 1]
+
         float top_x = center.x() + dimensions.x() / 2;
         float top_y = center.y() + dimensions.y() / 2;
         float top_z = center.z() + dimensions.z() / 2;
         float bottom_x = center.x() - dimensions.x() / 2;
         float bottom_y = center.y() - dimensions.y() / 2;
         float bottom_z = center.z() - dimensions.z() / 2;
+        float epsilon = std::numeric_limits<float>::epsilon();
         
         Eigen::Vector3f moveDir = mass.position - mass.last_position;
 
-        if (mass.last_position.x() >= top_x && mass.position.x() <= top_x) {
-            float t = (top_x - mass.last_position.x()) / moveDir.x();
-            float y = t * moveDir.y() + mass.last_position.y();
-            float z = t * moveDir.z() + mass.last_position.z();
-            if (y < top_y && y > bottom_y && z < top_z && z > bottom_z) {
-                // mass.position.x() = top_x;
-                mass.position = {top_x, y, z};
-                return;
+        if (moveDir.x() <= -epsilon)
+            if (mass.last_position.x() >= top_x && mass.position.x() <= top_x) {
+                float t = (top_x - mass.last_position.x()) / moveDir.x();
+                float y = t * moveDir.y() + mass.last_position.y();
+                float z = t * moveDir.z() + mass.last_position.z();
+                if (y <= top_y && y >= bottom_y && z <= top_z && z >= bottom_z) {
+                    mass.position.x() = top_x;
+                    mass.position.y() = friction * y + (1 - friction) * mass.position.y();
+                    mass.position.z() = friction * z + (1 - friction) * mass.position.z();
+                    // mass.position = {top_x, y, z};
+                    return;
+                }
             }
-        }
-        if (mass.last_position.x() <= bottom_x && mass.position.x() >= bottom_x) {
-            float t = (bottom_x - mass.last_position.x()) / moveDir.x();
-            float y = t * moveDir.y() + mass.last_position.y();
-            float z = t * moveDir.z() + mass.last_position.z();
-            if (y < top_y && y > bottom_y && z < top_z && z > bottom_z) {
-                // mass.position.x() = bottom_x;
-                mass.position = {bottom_x, y, z};
-                return;
+        if (moveDir.x() >= epsilon)
+            if (mass.last_position.x() <= bottom_x && mass.position.x() >= bottom_x) {
+                float t = (bottom_x - mass.last_position.x()) / moveDir.x();
+                float y = t * moveDir.y() + mass.last_position.y();
+                float z = t * moveDir.z() + mass.last_position.z();
+                if (y <= top_y && y >= bottom_y && z <= top_z && z >= bottom_z) {
+                    mass.position.x() = bottom_x;
+                    mass.position.y() = friction * y + (1 - friction) * mass.position.y();
+                    mass.position.z() = friction * z + (1 - friction) * mass.position.z();
+                    // mass.position = {bottom_x, y, z};
+                    return;
+                }
             }
-        }
-        if (mass.last_position.y() >= top_y && mass.position.y() <= top_y) {
-            float t = (top_y - mass.last_position.y()) / moveDir.y();
-            float x = t * moveDir.x() + mass.last_position.x();
-            float z = t * moveDir.z() + mass.last_position.z();
-            if (x < top_x && x > bottom_x && z < top_z && z > bottom_z) {
-                // mass.position.y() = top_y;
-                mass.position = {x, top_y, z};
-                return;
+        if (moveDir.y() <= -epsilon)
+            if (mass.last_position.y() >= top_y && mass.position.y() <= top_y) {
+                float t = (top_y - mass.last_position.y()) / moveDir.y();
+                float x = t * moveDir.x() + mass.last_position.x();
+                float z = t * moveDir.z() + mass.last_position.z();
+                if (x <= top_x && x >= bottom_x && z <= top_z && z >= bottom_z) {
+                    mass.position.x() = friction * x + (1 - friction) * mass.position.x();
+                    mass.position.y() = top_y;
+                    mass.position.z() = friction * z + (1 - friction) * mass.position.z();
+                    // mass.position = {x, top_y, z};
+                    return;
+                }
             }
-        }
-        if (mass.last_position.y() <= bottom_y && mass.position.y() >= bottom_y) {
-            float t = (bottom_y - mass.last_position.y()) / moveDir.y();
-            float x = t * moveDir.x() + mass.last_position.x();
-            float z = t * moveDir.z() + mass.last_position.z();
-            if (x < top_x && x > bottom_x && z < top_z && z > bottom_z) {
-                // mass.position.y() = bottom_y;
-                mass.position = {x, bottom_y, z};
-                return;
+        if (moveDir.y() >= epsilon)
+            if (mass.last_position.y() <= bottom_y && mass.position.y() >= bottom_y) {
+                float t = (bottom_y - mass.last_position.y()) / moveDir.y();
+                float x = t * moveDir.x() + mass.last_position.x();
+                float z = t * moveDir.z() + mass.last_position.z();
+                if (x <= top_x && x >= bottom_x && z <= top_z && z >= bottom_z) {
+                    mass.position.x() = friction * x + (1 - friction) * mass.position.x();
+                    mass.position.y() = bottom_y;
+                    mass.position.z() = friction * z + (1 - friction) * mass.position.z();
+                    // mass.position = {x, bottom_y, z};
+                    return;
+                }
             }
-        }
-        if (mass.last_position.z() >= top_z && mass.position.z() <= top_z) {
-            float t = (top_z - mass.last_position.z()) / moveDir.z();
-            float x = t * moveDir.x() + mass.last_position.x();
-            float y = t * moveDir.y() + mass.last_position.y();
-            if (x < top_x && x > bottom_x && y < top_y && y > bottom_y) {
-                // mass.position.z() = top_z;
-                mass.position = {x, y, top_z};
-                return;
+        if (moveDir.z() <= -epsilon)
+            if (mass.last_position.z() >= top_z && mass.position.z() <= top_z) {
+                float t = (top_z - mass.last_position.z()) / moveDir.z();
+                float x = t * moveDir.x() + mass.last_position.x();
+                float y = t * moveDir.y() + mass.last_position.y();
+                if (x <= top_x && x >= bottom_x && y <= top_y && y >= bottom_y) {
+                    mass.position.x() = friction * x + (1 - friction) * mass.position.x();
+                    mass.position.y() = friction * y + (1 - friction) * mass.position.y();
+                    mass.position.z() = top_z;
+                    // mass.position = {x, y, top_z};
+                    return;
+                }
             }
-        }
-        if (mass.last_position.z() <= bottom_z && mass.position.z() >= bottom_z) {
-            float t = (bottom_z - mass.last_position.z()) / moveDir.z();
-            float x = t * moveDir.x() + mass.last_position.x();
-            float y = t * moveDir.y() + mass.last_position.y();
-            if (x < top_x && x > bottom_x && y < top_y && y > bottom_y) {
-                // mass.position.z() = bottom_z;
-                mass.position = {x, y, bottom_z};
-                return;
+        if (moveDir.z() >= epsilon)
+            if (mass.last_position.z() <= bottom_z && mass.position.z() >= bottom_z) {
+                float t = (bottom_z - mass.last_position.z()) / moveDir.z();
+                float x = t * moveDir.x() + mass.last_position.x();
+                float y = t * moveDir.y() + mass.last_position.y();
+                if (x <= top_x && x >= bottom_x && y <= top_y && y >= bottom_y) {
+                    mass.position.x() = friction * x + (1 - friction) * mass.position.x();
+                    mass.position.y() = friction * y + (1 - friction) * mass.position.y();
+                    mass.position.z() = bottom_z;
+                    // mass.position = {x, y, bottom_z};
+                    return;
+                }
             }
-        }
+        // Failsafe
         mass.position = mass.last_position;
     }
 };
